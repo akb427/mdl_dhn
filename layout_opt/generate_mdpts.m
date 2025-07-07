297aad98586d0403a41aae45572910dcedb4e252
akb427 <akb427@gmail.com>
2025-07-07 15:15:29 -0400

    improve clarity

--- layout_opt/generate_mdpts.m
+++ layout_opt/generate_mdpts.m
@@ -1,37 +1,59 @@
-function [mdpts, users] = generate_mdpts(N)
-%UNTITLED2 Summary of this function goes here
-%   Detailed explanation goes here
+function [mdpts, users] = generate_mdpts(num_user)
+%GENERATE_MDPTS  Generates all potential midpoints for N users.
+%
+%   [mdpts, users] = GENERATE_MDPTS(num_users)
+%
+%   DESCRIPTION:
+%   Generate all midpoint of midpoints and users for a set of num_user
+%   points. Outputs the list of midpoints of midpoints and what users are
+%   contained in each midpoint.
+%
+%   INPUTS:
+%       num_users   - Integer number of users to be considered.
+%
+%   OUTPUTS:
+%       mdpts   - Matrix of midpoints described by row.
+%       users   - Binary matrix of users in each midpoint.
 
 %% Initialize Midpoints
-N = uint32(N);
-mdpts =uint32([(1:N)', zeros(N, N-1)]);
-users = logical(eye(N));
+num_user = uint32(num_user);
+mdpts =uint32([(1:num_user)', zeros(num_user, num_user-1)]);
+users = logical(eye(num_user));
 
 %% Call recursion
-i = uint32(2);
-while i<length(users)
-    u1 = users(i,:);
-    if any(u1==0)
-        addptsrc(u1,i,i)
+idx_mdpt = uint32(2);
+while idx_mdpt<length(users)
+    users_in_mdpt = users(idx_mdpt,:);
+    % If all users haven't been captured
+    if any(users_in_mdpt==0)
+        addpts_rc(users_in_mdpt,idx_mdpt,idx_mdpt)
     end
-    i = i+1;
+    idx_mdpt = idx_mdpt+1;
 end
 
-%% Recursive function
-    function  addptsrc(ui,mi,i)
-        idx = ~any(users(1:i-1,:)&ui,2);
-        if any(idx)
-            idxl = find(idx);
-            nm = numel(idxl);
-            ulist = ui|users(idx,:);
-            users(end+1:end+size(ulist,1),:) = ulist;
-            mlist = [repelem(mi, nm,1), idxl, zeros(nm,N-size(mi,2)-1)];
-            mdpts(end+1:end+size(ulist,1),:) = mlist;
-            for j = nm:-1:2
-                ui2 = ui|users(idxl(j),:);
-                addptsrc(ui2,[mi idxl(j)] ,idxl(j));
-            end
+%% Add existing midpoints to midpoint
+    function  addpts_rc(user_set_i,idx_mdpt1,idx_mdpt2)
+
+    % find midpoints with no overlapping users
+    idx_compatible = ~any(users(1:idx_mdpt2-1,:)&user_set_i,2);
+    if any(idx_compatible)
+        in_mdpt_list = find(idx_compatible);
+        num_mdpt = numel(in_mdpt_list);
+
+        % users in these new midpoints
+        users_new = user_set_i|users(idx_compatible,:);
+        users(end+1:end+size(users_new,1),:) = users_new;
+
+        % midpoint described by previous rows of midpoint list
+        mlist = [repelem(idx_mdpt1, num_mdpt,1), in_mdpt_list, zeros(num_mdpt,num_user-size(idx_mdpt1,2)-1)];
+        mdpts(end+1:end+size(users_new,1),:) = mlist;
+
+        % add additional feasible midpoints to the list
+        for idx_list = num_mdpt:-1:2
+            ui2 = user_set_i|users(in_mdpt_list(idx_list),:);
+            addpts_rc(ui2,[idx_mdpt1 in_mdpt_list(idx_list)] ,in_mdpt_list(idx_list));
         end
+    end
     end
 
 end

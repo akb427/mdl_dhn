222387c2e1d618b5aa3853f4afc70d4e7105441b
akb427 <akb427@gmail.com>
2025-07-21 15:41:29 -0400

    improver readability

--- layout_opt/final/bnb_length.m
+++ layout_opt/final/bnb_length.m
@@ -1,72 +1,107 @@
-function [tr_best, c_best] = bnb_length(pairs, users,c, n)
-%UNTITLED8 Summary of this function goes here
-%   Detailed explanation goes here
+function [tree_best, c_best] = bnb_length(pairs, users_in_node2, c_list, n)
+%BNB_LENGTH  Finds the length minimizing solution via bnb.
+%
+%   [tree_best, c_best] = BNB_LENGTH(pairs, users_in_node2, c_list, n)
+%
+%   DESCRIPTION:
+%   Briefly explain the purpose of the function, what it computes, or how it
+%   fits into the overall workflow. Mention any important assumptions or side
+%   effects (e.g., plotting, modifying global variables, saving files).
+%
+%   INPUTS:
+%       pairs   - Matrix of all midpoint pairs that don't overlap users.
+%       users_in_node2   - Users in the midpoint in the second column of pair
+%       c_list  - Vector of total distance cost of pairs
+%       n       - Structure of problem sizing
+%
+%   OUTPUTS:
+%       tree_best   - Length minimizing tree by connecting pairs
+%       c_best      - Length cost of best tree
+%
+%   SEE ALSO: cost_length.
 
+%% Bound with midpoint of all users
 
-%% Bound
-tr = zeros(n.u,2,100);
-ctr = NaN(1,100);
+% preallocate storage
+n_preallocate = 100;
+trees_all = zeros(n.u,2,n_preallocate);
+cost_trees = NaN(1,n_preallocate);
 
+% bound by pairs that have all users in second node
+plant2full = find(all(users_in_node2,2));
+[c_bound,idx_best] = min(c_list(plant2full));
+% store cost and tree
+cost_trees(1) = c_bound;
+trees_all(:,:,1) = [pairs(plant2full(idx_best),:); zeros(n.u-1,2)];
+idx_tree_new = 2;
 
-idx =find(all(users,2));
-[cb,idx_best] = min(c(idx));
-ctr(1) = cb;
-tr(:,:,1) = [pairs(idx(idx_best),:); zeros(n.u-1,2)];
-idxtn = 2;
-idx(idx_best) = [];
-pairs(idx,:) = [];
-users(idx,:) = [];
-c(idx,:) = [];
+% remove worse performing full pairs from lists
+plant2full(idx_best) = [];
+pairs(plant2full,:) = [];
+users_in_node2(plant2full,:) = [];
+c_list(plant2full,:) = [];
 
-% Eliminate already unfeasible points
-idx = c<=cb;
-pairs = pairs(idx,:);
-users = users(idx,:);
-c = c(idx,:);
+% eliminate already unfeasible points
+idx_feasible = c_list<=c_bound;
+pairs = pairs(idx_feasible,:);
+users_in_node2 = users_in_node2(idx_feasible,:);
+c_list = c_list(idx_feasible,:);
 
 %% Call recursion
+
+% index of last plant-connected node
 splt = find(pairs(:,1)==0,1,'last');
 
 % for all possible starting trees
-for i = 1:splt
-    trs = zeros(n.u,2);
-    trs(1,:) = pairs(i,:);
-    add_node(trs, users(i,:), c(i),1,i);
+for idx_pair_plant = 1:splt
+    % preallocate tree
+    tree_i = zeros(n.u,2);
+    tree_i(1,:) = pairs(idx_pair_plant,:);
+    add_node(tree_i, users_in_node2(idx_pair_plant,:), c_list(idx_pair_plant),1,idx_pair_plant);
 end
 
-[c_best, idx_best] = min(ctr);
-tr_best = tr(:,:,idx_best);
+%% Output Results
+
+[c_best, idx_best] = min(cost_trees);
+tree_best = trees_all(:,:,idx_best);
+
 %% Recursive Function
-    function add_node(tr1, u1, c1, p1, idx1)
-    % check stop
-    if all(u1)&&c1<cb
-        cb = c1;
-        ctr(1,idxtn) = c1; 
-        tr(:,:,idxtn) = tr1;
-        idxtn = idxtn+1;
-        if idxtn>100
-            warning('Not enough space preallocated in tree saver')
+    function add_node(tree, users_in_tree, cost, row_in_tree, newest_pairs_in_tree)
+    % check search termination conditions
+    if all(users_in_tree)&&cost<c_bound    % if a new bound has been found
+        % update bound and add tree to running storage
+        c_bound = cost;
+        cost_trees(1,idx_tree_new) = cost; 
+        trees_all(:,:,idx_tree_new) = tree;
+        idx_tree_new = idx_tree_new+1;
+        % if more trees have been explored than preallocated
+        if idx_tree_new > n_preallocate
+            warning('Not enough space preallocated in tree saver. Consider adding more preallocated space. Does not affect code operation')
         end
-    elseif c1<cb
-        % parallel but only from above
-        [idxa,~] = find(pairs(min(idx1),1)==pairs(1:min(idx1)-1,1));
-        idxa = idxa(~any(u1 & users(idxa,:),2))';
-        for ii = idxa
-            tr2 = tr1;
-            tr2(p1+1,:) = pairs(ii,:);
-            u2 = users(ii,:)|u1;
-            c2 = c1+c(ii);
-            add_node(tr2, u2, c2,p1+1,[idx1;ii]);
+    elseif cost<c_bound            % if the search should continue
+        % parallel (connected to the same start node) but only from above in pairs list
+        [pairs2add,~] = find(pairs(min(newest_pairs_in_tree),1)==pairs(1:min(newest_pairs_in_tree)-1,1));
+        % remove those with user overlap
+        pairs2add = pairs2add(~any(users_in_tree & users_in_node2(pairs2add,:),2))';
+        for idx_pair = pairs2add
+            % add node to tree
+            tree_new = tree;
+            tree_new(row_in_tree+1,:) = pairs(idx_pair,:);
+            users_in_tree_new = users_in_node2(idx_pair,:)|users_in_tree;
+            cost_new = cost+c_list(idx_pair);
+            % keep building tree
+            add_node(tree_new, users_in_tree_new, cost_new, row_in_tree+1, [newest_pairs_in_tree;idx_pair]);
         end
-        % children of newly added node in tree w
-        idxb = find(any(pairs(idx1,2)'==pairs(:,1),2));
-        idxb = idxb(~any(u1 & users(idxb,:),2))';
-        for ii = idxb
-            tr2 = tr1;
-            tr2(p1+1,:) = pairs(ii,:);
-            u2 = users(ii,:)|u1;
-            c2 = c1+c(ii);
-            add_node(tr2, u2, c2,p1+1,ii);
+        % children of newly added node in tree with no overlap
+        pairs2add = find(any(pairs(newest_pairs_in_tree,2)'== pairs(:,1),2));
+        pairs2add = pairs2add(~any(users_in_tree & users_in_node2(pairs2add,:),2))';
+        for idx_pair = pairs2add
+            % add node to tree
+            tree_new = tree;
+            tree_new(row_in_tree+1,:) = pairs(idx_pair,:);
+            users_in_tree_new = users_in_node2(idx_pair,:)|users_in_tree;
+            cost_new = cost+c_list(idx_pair);
+            add_node(tree_new, users_in_tree_new, cost_new,row_in_tree+1,idx_pair);
         end
     end
     end

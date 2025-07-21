f13c16a93aa4c385a8c79a28b9878d56e7df899a
akb427 <akb427@gmail.com>
2025-07-21 11:05:56 -0400

    function description and comments

--- layout_opt/locate_mdpts.m
+++ layout_opt/locate_mdpts.m
@@ -4,32 +4,25 @@ function [map] = locate_mdpts(mdpts,map, nu)
 %   [map] = locate_mdpts(mdpts,map, nu)
 %
 %   DESCRIPTION:
-%   Briefly explain the purpose of the function, what it computes, or how it
-%   fits into the overall workflow. Mention any important assumptions or side
-%   effects (e.g., plotting, modifying global variables, saving files).
+%   Get the x and y coordinates of all the midpoints. Locate the points
+%   averaged between all points that make up midpoint. Adds a dummy row to
+%   the map to account for the midpoints containing zero elements to
+%   maintain space efficiency of the mdpts variable, which is removed
+%   before outputing map.
 %
 %   INPUTS:
-%       in1  - Description of input 1 (type, format, units if applicable)
-%       in2  - Description of input 2
-%       ...  - Additional inputs as needed
+%       mdpts   - Matrix of midpoint components.
+%       map     - X and Y coordinates of the users.
+%       nu      - Number of users in the system.
 %
 %   OUTPUTS:
-%       out1 - Description of output 1 (what it represents)
-%       out2 - Description of output 2
-%       ...  - Additional outputs as needed
+%       map     - X and Y coordinates of the users and midpoints.
 %
-%   EXAMPLE USAGE:
-%       [best_part, results] = my_partition_solver(G, params);
-%
-%   DEPENDENCIES:
-%       List other custom functions this function calls, if any.
-%
-%   SEE ALSO:
-%       RelatedFunction1, RelatedFunction2
+%   SEE ALSO: generate_structure, create_map.
 
 %% Setup
 
-% Initialize storage
+% initialize storage with NaN for 0 points.
 map_x = [NaN; map(:,1); zeros(size(mdpts,1)-nu,1)];
 map_y = [NaN; map(:,2); zeros(size(mdpts,1)-nu,1)];
 
@@ -37,21 +30,27 @@ map_y = [NaN; map(:,2); zeros(size(mdpts,1)-nu,1)];
 idx_lower = nu+1;
 idx_upper = find(all(mdpts<=nu,2),1,'last');
 
-%%
+%% Locate midpoints in chunks
+
+% while there are still midpoints to locate
 while idx_lower<size(mdpts, 1)
+    % maps points to correct rows in map with NaN included
     idx_in_map = mdpts(idx_lower:idx_upper,:)+1;
-    % Average Location
+    % average Location
     pts_x = map_x(idx_in_map);
-    pts_x = mean(pts_x,2, 'omitnan');
+    pts_x = mean(pts_x,2, 'omitnan');   % ignore zeros in mdpts
     pts_y = map_y(idx_in_map);
-    pts_y = mean(pts_y,2, 'omitnan');
-    % Add to map list
+    pts_y = mean(pts_y,2, 'omitnan');   % ignore zeros in mdpts
+    % add to map list
     map_x(idx_lower+1:idx_upper+1) = pts_x;
     map_y(idx_lower+1:idx_upper+1) = pts_y;
-    % Increment midpoint set
+    % increment midpoint set
     idx_lower = idx_upper+1;
-    % where all midpoints have been located
+    % midpoints where all components have been located
     idx_upper = find(all(mdpts<=idx_upper,2),1,'last');
 end
+
+%% Remove auxilliary NaN row
 map = [map_x(2:end) map_y(2:end)];
+
 end

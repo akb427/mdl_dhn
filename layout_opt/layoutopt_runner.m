b4e0c8e961fcd43ecef7a8710e6b010e3feba38b
akb427 <akb427@gmail.com>
2025-07-23 15:16:01 -0400

    save location

--- layout_opt/layoutopt_runner.m
+++ layout_opt/layoutopt_runner.m
@@ -1,7 +1,10 @@
 %LAYOUTOPT_RUNNER  Finds optimal layout of users.
 %
 %   DESCRIPTION: Finds the length and energy reducing layouts of the users
-%   in parma8users.mat. Saves the results
+%   in parma8users.mat. Saves the results. The bounding cost in the
+%   enthalpy BnB relies on assuming a temperature drop. If this temperature 
+%   drop is too high, then the bound is incorrect, so the search is
+%   repeated until this assumption holds.
 %
 %   DEPENDENCIES: generate_structure, locate_mdpts, cost_length,
 %   bnb_length, cost_enthalpy, bnb_enthalpy, expand_tree, fincalc_enthalpy
@@ -38,7 +41,7 @@ map = params.mapb(2:end,:)-params.mapb(1,:);
 %% Generate Layouts
 
 % either load or create and then load structure
-struct_file = sprintf('%sstruct%dusers.mat', fullfile(pth, 'structure\'),n.u);
+struct_file = sprintf('%sstruct%dusers.mat', fullfile(pth, 'structure\'), n.u);
 if isfile(struct_file)
     load(struct_file, 'n','mdpts','users_in_mdpt','pairs','users_in_node2')
 else
@@ -58,7 +61,7 @@ params.mapb = [0,0; map];
 [l.tr, l.c_best] = bnb_length(pairs, users_in_node2, l.c_add, n);
 
 % Save results
-baseName = sprintf('results_l_%dusers', n.u);
+baseName = sprintf('%sresults_l_%dusers', fullfile(pth, 'case study\'), n.u);
 file_version = 0;
 filename = sprintf('%s.mat', baseName);
 % Find unique version
@@ -71,24 +74,22 @@ save(filename,'params', 'l')
 
 %% Enthalpy Drop Minimized Layout
 
-% Bounding cost relies on assuming a temperature drop. If this temperature
-% drop is too high, then the bound is incorrect, so the loop is repeated
-% until this assumption holds.
-
+% while the assumption has been violated
 dropact = params.x;
 while dropact>=params.x
     params.x = dropact;
     % calculate the enthalpy cost and relavent parameters of all midpoints
-    [e.c_list, e.tc] = cost_enthalpy(mdpts, users_in_mdpt, pairs, users_in_node2, params, n);
-    [e.tr, e.c_best] = bnb_enthalpy(pairs, users_in_node2, e.c_list, n, mdpts, params, e.tc);
+    [e.c_list, e.c_comp] = cost_enthalpy(mdpts, users_in_mdpt, pairs, users_in_node2, params, n);
+    [e.tr, e.c_best] = bnb_enthalpy(pairs, users_in_node2, e.c_list, n, mdpts, params, e.c_comp);
     [e.tr,n] = expand_tree(e.tr, mdpts, n, 0);
-    [dE, dropact] = fincalc_enthalpy(e.tr, n, params, e.tc, mdpts, pairs);
+    [dE, dropact] = fincalc_enthalpy(e.tr, n, params, e.c_comp, mdpts, pairs);
 end
 
 % Save results
+baseName = sprintf('%sresults_e_%dusers', fullfile(pth, 'case study\'), n.u);
 if file_version==0
     filename = ['results_e', num2str(n.u), 'users.mat'];
 else
-    filename = ['results_e', num2str(n.u), 'users',num2str(file_version),'.mat'];
+    filename = sprintf('%s%d.mat', baseName, file_version);
 end
 save(filename,'params','e')

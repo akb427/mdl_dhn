b214f556f4fe797309f79662af18ed575b661115
akb427 <akb427@gmail.com>
2025-07-23 14:28:06 -0400

    improve readability

--- layout_opt/solve bnb/fincalc_enthalpy.m
+++ layout_opt/solve bnb/fincalc_enthalpy.m
@@ -1,75 +1,125 @@
-function [dE,dTmax] = fincalc_enthalpy(G, n, params, tc, mdpts, pairs)
-%FINCALC_ENTHALPY Summary of this function goes here
-%   Detailed explanation goes here
+function [dE,dTmax] = fincalc_enthalpy(tree, n, params, tc, mdpts, pairs)
+%FINCALC_ENTHALPY  Calculate enthalpy loss in full tree.
+%
+%   [dE,dTmax] = FINCALC_ENTHALPY(tree, n, params, tc, mdpts, pairs)
+%
+%   DESCRIPTION:
+%   Calculates the true enthalpy loss in an expanded tree with all users
+%   connected. Gets all the relavent variables for the calculation in the
+%   expanded tree matrix and then calculates the temperature drop in each
+%   pipe. Also outputs the maximum temperature drop to confirm the
+%   assumption on the lower bound is acceptable. 
+%
+%   INPUTS:
+%       tree    - Matrix of complete, expanded tree to be evaluated 
+%       n       - Structure of problem sizing
+%       params  - Structure of system parameters.
+%       tc      - Structure of element parameters.
+%       mdpts   - Matrix of midpoint components.
+%       pairs   - Matrix of all midpoint pairs that don't overlap users.
+%
+%   OUTPUTS:
+%       dE      - Total network losses
+%       dTmax   - Maximum temperature drop in an edge
+%   
+%   SEE ALSO: expand_tree
 
-%% Calculations
-G =[G, zeros(n.u+n.s,8)];
-idx = ~any(G(:,2)==G(:,1)',2);
-G(:,3) = idx;
-calc_users(unique(G(idx,1)'))
+%% Network parameters
 
-G(:,4) = params.cp*G(:,3)*tc.mi;
-idx = G(:,3)>1;
-G(idx,5) = params.DL;
-G(~idx,5) = params.Ds;
+% preallocate tree [v_start v_end u_ds mdotcp D L hAs Tin T]
+tree =[tree, zeros(n.u+n.s,8)];
+
+% number of downstream users
+idx_term = ~any(tree(:,2)==tree(:,1)',2);
+tree(:,3) = idx_term;
+calc_users(unique(tree(idx_term,1)'))
+
+% Assign mdotcp and diameters
+tree(:,4) = params.cp*tree(:,3)*tc.mi;
+many_users = tree(:,3)>1;
+tree(many_users,5) = params.DL;
+tree(~many_users,5) = params.Ds;
+
 tic
-for i = 1:n.u+n.s
-    ni = G(i,1);
-    if ni~=0
-        idx = mdpts(ni,:)==G(i,2);
-        if any(idx)
-            G(i,6) = tc.L_mdpts(ni,idx);
-        else
-            G(i,6) = tc.L_pairs(all(pairs==G(i,1:2),2));
+% assign lengths
+for row = 1:n.u+n.s
+    v_start = tree(row,1);
+    if v_start~=0 % if it is not the plant
+        node_match = mdpts(v_start,:)==tree(row,2);
+        if any(node_match) % if this connection is from the midpoint design, length comes from precalc
+            tree(row,6) = tc.L_mdpts(v_start,node_match);
+        else % length is only of connection
+            idx_in_pair = all(pairs==tree(row,1:2),2);
+            tree(row,6) = tc.L_pairs(idx_in_pair);
         end
-    else
-        G(i,6) = tc.L_pairs(all(pairs==G(i,1:2),2));
+    else % if it is the plant, length is only of connection
+        idx_in_pair = all(pairs==tree(row,1:2),2);
+        tree(row,6) = tc.L_pairs(idx_in_pair);
     end
 end
-G(:,7)= params.h*pi*G(:,5).*G(:,6);
+% calculate heat transfer coefficient
+tree(:,7)= params.h*pi*tree(:,5).*tree(:,6);
 
-idx = G(:,1)==0;
-G(idx,8) = params.Ts;
-calc_dT(idx)
+% Assign inlet temp to plant edges
+idx_root = tree(:,1)==0;
+tree(idx_root,8) = params.Ts;
 
-G(:,10) = G(:,4).*(G(:,8)-G(:,9));
-dE = sum(G(:,10));
-dTmax = max(params.Ts-G(:,9));
+%% Calculate temperatures and losses
 
+calc_dT(idx_root)
+
+% Losses = hAs(Tin-T)
+tree(:,10) = tree(:,4).*(tree(:,8)-tree(:,9));
+% Total losses
+dE = sum(tree(:,10));
+% maximum temperature drop for lower bound calculation
+dTmax = max(params.Ts-tree(:,9));
 
 %% Functions
 
-    function calc_users(nd)
-    hold = NaN(1,n.u);
-    idx2 = 1;
-    for ii = nd    
-        idx1 = G(:,1)==ii;
-        if all(G(idx1,3)>=1)
-            idx3 = G(:,2)==ii;
-            G(idx3,3) = sum(G(idx1,3))+(G(idx3,2)<=n.u);
-            idx3 = G(idx3&G(:,1)>0,1);
-            if ~isempty(idx3)
-                hold(idx2) = idx3;
-                idx2 = idx2+1;
+    function calc_users(v)
+    % calculate the number of downstream users through recursive counting of nodes, roots up.
+    tbe = NaN(1,n.u);
+    idx_in_tbe = 1;
+    for vi = v    
+        start_rows = tree(:,1)==vi;
+        if all(tree(start_rows,3)>=1) % if we know how many users downstream
+            end_rows = tree(:,2)==vi;
+            % # users = users eventually downstream + directly connected
+            tree(end_rows,3) = sum(tree(start_rows,3))+(tree(end_rows,2)<=n.u);
+            % add nodes to which it is connected to tbe
+            start_node = tree(end_rows&tree(:,1)>0,1);
+            if ~isempty(start_node)
+                tbe(idx_in_tbe) = start_node;
+                idx_in_tbe = idx_in_tbe+1;
             end
-        else
-            hold(idx2) = ii;
-            idx2 = idx2+1;
+        else    % if it still needs to be explored
+            tbe(idx_in_tbe) = vi;
+            idx_in_tbe = idx_in_tbe+1;
         end
     end
-    if ~all(isnan(hold))
-        hold = hold(~isnan(hold));
-        calc_users(unique(hold));
+    % if futher nodes need to be evaluated
+    if ~all(isnan(tbe))
+        tbe = tbe(~isnan(tbe));
+        calc_users(unique(tbe));
     end
     end
 
-    function calc_dT(idx1)
-        G(idx1,9) = G(idx1,4)./(G(idx1,4)+G(idx1,7)).*G(idx1,8)+G(idx1,7)./(G(idx1,4)+G(idx1,7))*params.Ta;
-        for ii = find(idx1)'
-            idx2 = G(:,1)==G(ii,2);
-            if any(idx2)
-                G(idx2,8) = G(ii,9);
-                calc_dT(idx2)
+    function calc_dT(row_idx)
+    % recursively calculate the outlet/bulk temperature in all the pipes
+    % based on the inlet temperatures.
+        % outlet temperatures [mc_p/(mc_p+hAs)*T_in]+[hA_s/(mc_p+hAs)*T_a]
+        term1 = tree(row_idx,4)./(tree(row_idx,4)+tree(row_idx,7)).*tree(row_idx,8);
+        term2 = tree(row_idx,7)./(tree(row_idx,4)+tree(row_idx,7))*params.Ta;
+        tree(row_idx,9) = term1+term2;
+        % for all the rows with added outlet temp
+        for row_num = find(row_idx)'
+            % rows with outlet as inlet
+            row_idx_new = tree(:,1)==tree(row_num,2);
+            if any(row_idx_new)
+                % solve new outlet temp
+                tree(row_idx_new,8) = tree(row_num,9);
+                calc_dT(row_idx_new)
             end
         end
     end

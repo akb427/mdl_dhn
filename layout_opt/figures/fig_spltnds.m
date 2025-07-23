d5a52dab92e153158dfd08765a0c1e07d1f07601
akb427 <akb427@gmail.com>
2025-07-23 15:16:28 -0400

    readability

--- layout_opt/figures/fig_spltnds.m
+++ layout_opt/figures/fig_spltnds.m
@@ -1,8 +1,15 @@
 function fig_spltnds
-%FIG_SPLTNDS Summary of this function goes here
-%   Detailed explanation goes here
+%FIG_SPLTNDS  Illustrative plot of split node types.
+%
+%   [out1, out2] = FUNCTION_NAME(in1, in2)
+%
+%   DESCRIPTION: 
+%   Plots a simple node (of only users), complex (of only
+%   midpoints), and mixed (of both users and midpoints) for illustrative
+%   purposes.
 
-%% Make Graphs
+%% Make Sample Graphs
+
 G{1} = graph([1 6 6 6 6],[6 2 3 4 5]);
 G{2} = graph([1 8 8 6 6 7 7],[8 6 7 2 3 4 5]);
 G{3} = graph([1 7 6 6 7 7],[7 6 2 3 4 5]);
@@ -28,7 +35,8 @@ lbl{3} = ["",string(1:4),"",""];
 name{1} = 'Simple';
 name{2} = 'Complex';
 name{3} = 'Mixed';
-%% Plot
+
+%% Plot three sample node types
 
 mkr = {'diamond','o','square'};
 clr = [[0.6350 0.0780 0.1840];[0 0.4470 0.7410];[0.4660 0.6740 0.1880]];
@@ -38,30 +46,24 @@ set(gcf,'Position',[2303,583,457,173])
 
 tiledlayout(1,3,'TileSpacing','compact','Padding','compact')
 
-for i = 1:3
+for idx_plot = 1:3
     nexttile
-    if i==3
+    if idx_plot == 3
         hold on
-        for j =1:3
-            plot(nan, nan,'Marker',mkr(j),'MarkerFaceColor', clr(j,:),'MarkerEdgeColor', clr(j,:),'LineStyle','none','MarkerSize',7)
+        for idx_node = 1:3
+            plot(nan, nan,'Marker',mkr(idx_node),'MarkerFaceColor', clr(idx_node,:),'MarkerEdgeColor', clr(idx_node,:),'LineStyle','none','MarkerSize',7)
         end
         L = legend('Plant\quad', 'Node\quad', 'User','AutoUpdate','off','FontSize',11,'Orientation','horizontal');
         L.ItemTokenSize(1) = 9;
         L.Layout.Tile = 'South';
     end
     hold on
-    plot(G{i}, 'Xdata',x{i},'Ydata',y{i},'NodeLabel',lbl{i} ,'marker', mkr(idx{i}), 'NodeColor', clr(idx{i},:), 'EdgeColor','k','interpreter','latex','NodeFontSize',12,'Linewidth',1,'MarkerSize',7)
+    plot(G{idx_plot}, 'Xdata',x{idx_plot},'Ydata',y{idx_plot},'NodeLabel',lbl{idx_plot} ,'marker', mkr(idx{idx_plot}), 'NodeColor', clr(idx{idx_plot},:), 'EdgeColor','k','interpreter','latex','NodeFontSize',12,'Linewidth',1,'MarkerSize',7)
     box on
     set(gca,'xtick',[],'ytick',[])
-    xlabel(name{i},'FontSize',12,'FontWeight','bold')
+    xlabel(name{idx_plot},'FontSize',12,'FontWeight','bold')
     hold off
 end
-
-%%
-
-
-%L.Units = 'pixel';
-%L.Position(3) = 260;
 
 
 end
